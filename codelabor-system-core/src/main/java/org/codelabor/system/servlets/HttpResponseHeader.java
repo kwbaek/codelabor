4df501f4b27d1152495e1b3f0c02ce762f16422d
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-08-03 04:06:15 +0000

    header fields 추가

---
+++ codelabor-system-core/src/main/java/org/codelabor/system/servlets/HttpResponseHeader.java
@@ -0,0 +1,33 @@
+package org.codelabor.system.servlets;
+
+public class HttpResponseHeader {
+	public final static String ACCEPT_RANGES = "Accept-Ranges";
+	public final static String AGE = "Age";
+	public final static String ALLOW = "Allow";
+	public final static String CACHE_CONTROL = "Cache-Control";
+	public final static String CONTENT_ENCODING = "Content-Encoding";
+	public final static String CONTENT_LANGUAGE = "Content-Language";
+	public final static String CONTENT_LENGTH = "Content-Length";
+	public final static String CONTENT_LOCATION = "Content-Location";
+	public final static String CONTENT_DISPOSITION = "Content-Disposition";
+	public final static String CONTENT_MD5 = "Content-MD5";
+	public final static String CONTENT_RANGE = "Content-Range";
+	public final static String CONTENT_TYPE = "Content-Type";
+	public final static String DATE = "Date";
+	public final static String ETAG = "ETag";
+	public final static String EXPIRES = "Expires";
+	public final static String LAST_MODIFIED = "Last-Modified";
+	public final static String LOCATION = "Location";
+	public final static String PRAGMA = "Pragma";
+	public final static String PROXY_AUTHENTICATE = "Proxy-Authenticate";
+	public final static String REFRESH = "Refresh";
+	public final static String RETRY_AFTER = "Retry-After";
+	public final static String SERVER = "Server";
+	public final static String SET_COOKIE = "Set-Cookie";
+	public final static String TRAILER = "Trailer";
+	public final static String TRANSFER_ENCODING = "Transfer-Encoding";
+	public final static String VARY = "Vary";
+	public final static String VIA = "Via";
+	public final static String WARNING = "Warning";
+	public final static String WWW_AUTHENTICATE = "	WWW-Authenticate";
+}
