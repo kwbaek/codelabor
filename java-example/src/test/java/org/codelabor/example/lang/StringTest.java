6ef3ef07b16948d1509b3448a7f66589b880f6bf
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-08-12 03:06:17 +0000

    testSubstring 추가

--- java-example/src/test/java/org/codelabor/example/lang/StringTest.java
+++ java-example/src/test/java/org/codelabor/example/lang/StringTest.java
@@ -1,5 +1,6 @@
 package org.codelabor.example.lang;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class StringTest extends TestCase {
@@ -9,8 +10,17 @@ public class StringTest extends TestCase {
 		String filePatternString = "jsp/test.jsp images/test.gif; css/test.css, js/test.js";
 		String regexpPattern = "([ ]*[,; ][ ]*)";
 		String[] filePatterns = filePatternString.split(regexpPattern);
+		Assert.assertEquals(3, filePatterns.length);
 		for (String filePattern : filePatterns) {
 			System.out.println(filePattern);
 		}
+	}
+
+	public void testSubstring() {
+		String fileName = "/qwer/qwer/qwer/test.txt";
+		int index = fileName.lastIndexOf(".");
+		String extension = fileName.substring(index + 1);
+		Assert.assertEquals("txt", extension);
+		System.out.println(extension);
 	}
 }
