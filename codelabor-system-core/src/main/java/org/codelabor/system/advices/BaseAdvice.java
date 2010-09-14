49427411e934b07be13848a169d1a3de2ddcbe5a
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-14 11:07:41 +0000

    javadoc 작업

--- codelabor-system-core/src/main/java/org/codelabor/system/advices/BaseAdvice.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/advices/BaseAdvice.java
@@ -24,44 +24,94 @@ import org.springframework.context.MessageSource;
 import anyframe.core.properties.IPropertiesService;
 
 /**
+ * 기본 어드바이스
+ * 
  * @author Shin Sangjae
  * 
  */
 public class BaseAdvice {
 
+	/**
+	 * 로거
+	 */
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	/**
+	 * 로거명
+	 */
 	protected String loggerName = this.getClass().getName();
 
+	/**
+	 * 메시지 소스
+	 */
 	protected MessageSource messageSource;
 
+	/**
+	 * 프로퍼티 서비스
+	 */
 	protected IPropertiesService propertiesService;
 
+	/**
+	 * 생성자
+	 */
 	public BaseAdvice() {
 		super();
 	}
 
+	/**
+	 * 로거명을 가져온다.
+	 * 
+	 * @return 로거명
+	 */
 	public String getLoggerName() {
 		return loggerName;
 	}
 
+	/**
+	 * 메시지 소스를 가져온다.
+	 * 
+	 * @return 메시지 소스
+	 */
 	public MessageSource getMessageSource() {
 		return messageSource;
 	}
 
+	/**
+	 * 프로퍼티 서비스를 가져온다.
+	 * 
+	 * @return 프로퍼티 서비스
+	 */
 	public IPropertiesService getPropertiesService() {
 		return propertiesService;
 	}
 
+	/**
+	 * 로거명을 설정한다.
+	 * 
+	 * @param loggerName
+	 *            로거명
+	 */
 	public void setLoggerName(String loggerName) {
 		this.loggerName = loggerName;
 		this.logger = LoggerFactory.getLogger(loggerName);
 	}
 
+	/**
+	 * 메시지 소스를 설정한다.
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 */
 	public void setMessageSource(MessageSource messageSource) {
 		this.messageSource = messageSource;
 	}
 
+	/**
+	 * 프로퍼티 서비스를 설정한다.
+	 * 
+	 * @param propertiesService
+	 *            프로퍼티 서비스
+	 */
 	public void setPropertiesService(IPropertiesService propertiesService) {
 		this.propertiesService = propertiesService;
 	}
