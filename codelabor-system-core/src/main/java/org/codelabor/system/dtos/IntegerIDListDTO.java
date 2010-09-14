582d4573c2c5bd79b36754b40de19a2658dac43e
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-14 11:47:04 +0000

    apache 라이센스 정보 추가

--- codelabor-system-core/src/main/java/org/codelabor/system/dtos/IntegerIDListDTO.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/dtos/IntegerIDListDTO.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.codelabor.system.dtos;
 
 import java.io.Serializable;
@@ -5,7 +22,7 @@ import java.util.List;
 
 public class IntegerIDListDTO implements Serializable {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 4384445118698439928L;
 	private List<Integer> id;
@@ -19,23 +36,20 @@ public class IntegerIDListDTO implements Serializable {
 	}
 
 	/**
-	 * Constructs a <code>String</code> with all attributes
-	 * in name = value format.
-	 *
-	 * @return a <code>String</code> representation 
-	 * of this object.
+	 * Constructs a <code>String</code> with all attributes in name = value
+	 * format.
+	 * 
+	 * @return a <code>String</code> representation of this object.
 	 */
-	public String toString()
-	{
-	    final String TAB = "    ";
-	
-	    StringBuilder retValue = new StringBuilder();
-	    
-	    retValue.append("IntegerIDListDTO ( ")
-	        .append(super.toString()).append(TAB)
-	        .append("id = ").append(this.id).append(TAB)
-	        .append(" )");
-	    
-	    return retValue.toString();
+	@Override
+	public String toString() {
+		final String TAB = "    ";
+
+		StringBuilder retValue = new StringBuilder();
+
+		retValue.append("IntegerIDListDTO ( ").append(super.toString()).append(
+				TAB).append("id = ").append(this.id).append(TAB).append(" )");
+
+		return retValue.toString();
 	}
 }
