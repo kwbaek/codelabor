a0f5b41a6e666210838c23c183ab014e54b3c112
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-16 00:43:19 +0000

    javadoc 작업

--- codelabor-system-core/src/main/java/org/codelabor/system/exceptions/CommonException.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/exceptions/CommonException.java
@@ -23,25 +23,52 @@ import anyframe.common.exception.BaseException;
 import anyframe.common.exception.message.Message;
 
 /**
+ * 공통 예외 클래스
+ * 
  * @author Shin Sangjae
  * 
  */
 public abstract class CommonException extends BaseException {
 
 	/**
-	 *
+	 * 시리얼 버전 UID
 	 */
 	private static final long serialVersionUID = -2522334283452895450L;
+	/**
+	 * 메시지 코드
+	 */
 	private String messageCode;
 
+	/**
+	 * 생성자
+	 */
 	public CommonException() {
 		super();
 	}
 
+	/**
+	 * 메시지 코드
+	 * 
+	 * @param messageCode
+	 */
 	public void setMessageCode(String messageCode) {
 		this.messageCode = messageCode;
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param defaultMessage
+	 *            기본 메시지
+	 * @param wrappedException
+	 *            예외
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			Object[] messageParameters, String defaultMessage,
 			Throwable wrappedException) {
@@ -49,87 +76,231 @@ public abstract class CommonException extends BaseException {
 				wrappedException);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param defaultMessage
+	 *            기본 메시지
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			Object[] messageParameters, String defaultMessage) {
 		super(messageSource, messageKey, messageParameters, defaultMessage);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param wrappedException
+	 *            예외
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			Object[] messageParameters, Throwable wrappedException) {
 		super(messageSource, messageKey, messageParameters, wrappedException);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			Object[] messageParameters) {
 		super(messageSource, messageKey, messageParameters);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param defaultMessage
+	 *            기본 메시지
+	 * @param wrappedException
+	 *            예외
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			String defaultMessage, Throwable wrappedException) {
 		super(messageSource, messageKey, defaultMessage, wrappedException);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param defaultMessage
+	 *            기본 메시지
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			String defaultMessage) {
 		super(messageSource, messageKey, defaultMessage);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param wrappedException
+	 *            예외
+	 */
 	public CommonException(MessageSource messageSource, String messageKey,
 			Throwable wrappedException) {
 		super(messageSource, messageKey, wrappedException);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 */
 	public CommonException(MessageSource messageSource, String messageKey) {
 		super(messageSource, messageKey);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param wrappedException
+	 *            예외
+	 */
 	public CommonException(String messageKey, Object[] messageParameters,
 			Throwable wrappedException) {
 		super(messageKey, messageParameters, wrappedException);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 */
 	public CommonException(String messageKey, Object[] messageParameters) {
 		super(messageKey, messageParameters);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 * @param wrappedException
+	 *            예외
+	 */
 	public CommonException(String messageKey, Throwable wrappedException) {
 		super(messageKey, wrappedException);
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 */
 	public CommonException(String messageKey) {
 		super(messageKey);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Throwable#getMessage()
+	 */
 	@Override
 	public String getMessage() {
 		return super.getMessage();
 	}
 
+	/**
+	 * 메시지 키를 가져온다.
+	 * 
+	 * @return 메시지 키
+	 */
 	@Override
 	public String getMessageKey() {
 		return super.getMessageKey();
 	}
 
+	/**
+	 * 메시지 파라미터를 가져온다.
+	 * 
+	 * @return 메시지 파라미터 배열
+	 */
 	@Override
 	public Object[] getMessageParameters() {
 		return super.getMessageParameters();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * anyframe.common.exception.message.AnyFrameMessageSource#getMessages()
+	 */
 	@Override
 	public Message getMessages() {
 		return super.getMessages();
 	}
 
+	/**
+	 * 메시지 키를 설정한다.
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 */
 	@Override
 	public void setMessageKey(String messageKey) {
 		super.setMessageKey(messageKey);
 	}
 
+	/**
+	 * 메시지 파라미터를 설정한다.
+	 * 
+	 * @param param
+	 *            메시지 파라미터 배열
+	 */
 	@Override
 	public void setMessageParameters(Object[] param) {
 		super.setMessageParameters(param);
 	}
 
+	/**
+	 * 메시지 코드를 가져온다.
+	 * 
+	 * @return 메시지 코드
+	 */
 	public String getMessageCode() {
 		return this.messageCode;
 	}
