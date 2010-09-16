1fddebcae31892ea1d64c069fb8bfe3325562e68
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-16 01:31:30 +0000

    javadoc 작업

--- codelabor-system-core/src/main/java/org/codelabor/system/exceptions/NoRollbackCommonException.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/exceptions/NoRollbackCommonException.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,17 +20,41 @@ package org.codelabor.system.exceptions;
 import org.springframework.context.MessageSource;
 
 /**
+ * 롤백을 하지 않는 예외를 위한 공통 예외 클래스
+ * 
  * @author Shin Sangjae
  * 
  */
 public class NoRollbackCommonException extends CommonException implements
 		NoRollbackable {
 
+	/**
+	 * 시리얼 버전 UID
+	 */
+	private static final long serialVersionUID = -3414460040784097981L;
+
+	/**
+	 * 생성자
+	 */
 	public NoRollbackCommonException() {
 		super();
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param defaultMessage
+	 *            기본 메시지
+	 * @param wrappedException
+	 *            예외
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, Object[] messageParameters,
 			String defaultMessage, Throwable wrappedException) {
@@ -39,12 +63,36 @@ public class NoRollbackCommonException extends CommonException implements
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param defaultMessage
+	 *            기본 메시지
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, Object[] messageParameters, String defaultMessage) {
 		super(messageSource, messageKey, messageParameters, defaultMessage);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param wrappedException
+	 *            예외
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, Object[] messageParameters,
 			Throwable wrappedException) {
@@ -52,62 +100,139 @@ public class NoRollbackCommonException extends CommonException implements
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, Object[] messageParameters) {
 		super(messageSource, messageKey, messageParameters);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param defaultMessage
+	 *            기본 메시지
+	 * @param wrappedException
+	 *            예외
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, String defaultMessage, Throwable wrappedException) {
 		super(messageSource, messageKey, defaultMessage, wrappedException);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param defaultMessage
+	 *            기본 메시지
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, String defaultMessage) {
 		super(messageSource, messageKey, defaultMessage);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 * @param wrappedException
+	 *            예외
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey, Throwable wrappedException) {
 		super(messageSource, messageKey, wrappedException);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 * @param messageKey
+	 *            메시지 키
+	 */
 	public NoRollbackCommonException(MessageSource messageSource,
 			String messageKey) {
 		super(messageSource, messageKey);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 * @param wrappedException
+	 *            예외
+	 */
 	public NoRollbackCommonException(String messageKey,
 			Object[] messageParameters, Throwable wrappedException) {
 		super(messageKey, messageParameters, wrappedException);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 * @param messageParameters
+	 *            메시지 파라미터
+	 */
 	public NoRollbackCommonException(String messageKey,
 			Object[] messageParameters) {
 		super(messageKey, messageParameters);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 * @param wrappedException
+	 *            예외
+	 */
 	public NoRollbackCommonException(String messageKey,
 			Throwable wrappedException) {
 		super(messageKey, wrappedException);
 
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param messageKey
+	 *            메시지 키
+	 */
 	public NoRollbackCommonException(String messageKey) {
 		super(messageKey);
 
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3414460040784097981L;
 
 }
--- codelabor-system-core/src/main/java/org/codelabor/system/exceptions/ParameterNotFoundException.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/exceptions/ParameterNotFoundException.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,33 +18,54 @@
 package org.codelabor.system.exceptions;
 
 /**
+ * 필요한 파라미터가 없을 경우 발생하는 예외
+ * 
  * @author Shin Sangjae
  * 
  */
 public class ParameterNotFoundException extends Exception {
 
 	/**
-	 * 
+	 * 시리얼 버전 UID
 	 */
 	private static final long serialVersionUID = -3336288578034952316L;
 
+	/**
+	 * 생성자
+	 */
 	public ParameterNotFoundException() {
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param message
+	 *            메시지
+	 */
 	public ParameterNotFoundException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param cause
+	 *            원인
+	 */
 	public ParameterNotFoundException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 생성자
+	 * 
+	 * @param message
+	 *            메시지
+	 * @param cause
+	 *            원인
+	 */
 	public ParameterNotFoundException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
