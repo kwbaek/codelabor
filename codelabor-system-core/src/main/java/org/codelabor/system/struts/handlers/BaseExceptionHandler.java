78d9451cc2d33c5ce6c6cdbcaddbadd133f7ceed
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-19 07:43:28 +0000

    javadoc 작업

--- codelabor-system-core/src/main/java/org/codelabor/system/struts/handlers/BaseExceptionHandler.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/struts/handlers/BaseExceptionHandler.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.codelabor.system.struts.handlers;
 
 import org.apache.struts.action.ExceptionHandler;
@@ -7,34 +24,86 @@ import org.springframework.context.MessageSource;
 
 import anyframe.core.properties.IPropertiesService;
 
+/**
+ * 
+ * 기본 예외 핸들러 클래스
+ * 
+ * @author Shin Sangjae
+ * 
+ */
 public class BaseExceptionHandler extends ExceptionHandler {
 
+	/**
+	 * 로거
+	 */
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+	/**
+	 * 로거명
+	 */
 	protected String loggerName = this.getClass().getName();
+	/**
+	 * 메시지 소스
+	 */
 	protected MessageSource messageSource;
+	/**
+	 * 프로퍼티 서비스
+	 */
 	protected IPropertiesService propertiesService;
 
+	/**
+	 * 로거명을 가져온다.
+	 * 
+	 * @return 로거명
+	 */
 	public String getLoggerName() {
 		return loggerName;
 	}
 
+	/**
+	 * 메시지 소스를 가져온다.
+	 * 
+	 * @return 메시지 소스
+	 */
 	public MessageSource getMessageSource() {
 		return messageSource;
 	}
 
+	/**
+	 * 프로퍼티 서비스를 가져온다.
+	 * 
+	 * @return 프로퍼티 서비스
+	 */
 	public IPropertiesService getPropertiesService() {
 		return propertiesService;
 	}
 
+	/**
+	 * 로거명을 설정한다.
+	 * 
+	 * @param loggerName
+	 *            로거명
+	 */
 	public void setLoggerName(String loggerName) {
 		this.loggerName = loggerName;
 		this.logger = LoggerFactory.getLogger(loggerName);
 	}
 
+	/**
+	 * 메시지 소스를 설정한다.
+	 * 
+	 * @param messageSource
+	 *            메시지 소스
+	 */
 	public void setMessageSource(MessageSource messageSource) {
 		this.messageSource = messageSource;
 	}
 
+	/**
+	 * 프로퍼티 서비스를 설정한다.
+	 * 
+	 * @param propertiesService
+	 *            프로퍼티 서비스
+	 */
 	public void setPropertiesService(IPropertiesService propertiesService) {
 		this.propertiesService = propertiesService;
 	}
