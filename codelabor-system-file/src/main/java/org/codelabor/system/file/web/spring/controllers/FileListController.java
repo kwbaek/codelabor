e4861858655c542521c9b29194fc1aee69545b2c
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-23 00:14:33 +0000

    refectoring

---
+++ codelabor-system-file/src/main/java/org/codelabor/system/file/web/spring/controllers/FileListController.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codelabor.system.file.web.spring.controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.codelabor.system.file.FileConstants;
+import org.codelabor.system.file.RepositoryType;
+import org.codelabor.system.file.dtos.FileDTO;
+import org.codelabor.system.web.utils.RequestUtils;
+import org.springframework.web.servlet.ModelAndView;
+
+public class FileListController extends BaseFileController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		Map<String, Object> paramMap = RequestUtils.getParameterMap(request);
+		logger.debug(paramMap);
+
+		String mapId = (String) paramMap.get("mapId");
+		String repositoryType = (String) paramMap.get("repositoryType");
+
+		List<FileDTO> fileDTOList = null;
+		ModelAndView mav = null;
+		try {
+			if (StringUtils.isEmpty(repositoryType)) {
+				if (StringUtils.isEmpty(mapId)) {
+					fileDTOList = fileManager.selectFile();
+				} else {
+					fileDTOList = fileManager.selectFileByMapId(mapId);
+				}
+			} else {
+				switch (RepositoryType.valueOf(repositoryType)) {
+				case DATABASE:
+					fileDTOList = fileManager
+							.selectFileByRepositoryType(RepositoryType.DATABASE);
+					break;
+				case FILE_SYSTEM:
+					fileDTOList = fileManager
+							.selectFileByRepositoryType(RepositoryType.FILE_SYSTEM);
+					break;
+				}
+			}
+			mav = new ModelAndView(getSuccessView());
+			mav.addObject(FileConstants.FILE_LIST_KEY, fileDTOList);
+			mav.addObject(FileConstants.MAP_ID, mapIdGenerationService
+					.getNextStringId());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mav;
+	}
+}
