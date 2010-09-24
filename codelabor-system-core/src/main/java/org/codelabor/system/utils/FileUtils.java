5904fc6814891ea76da7f27912d9871223a17ff0
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-09-24 00:35:08 +0000

    method 추가

--- codelabor-system-core/src/main/java/org/codelabor/system/utils/FileUtils.java
+++ codelabor-system-core/src/main/java/org/codelabor/system/utils/FileUtils.java
@@ -1,5 +1,8 @@
 package org.codelabor.system.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 파일 유틸
  * 
@@ -8,18 +11,39 @@ package org.codelabor.system.utils;
  */
 public class FileUtils {
 	/**
-	 * 확장자를 가져온다.
+	 * 로거
+	 */
+	static Logger logger = LoggerFactory.getLogger(FileUtils.class);
+
+	/**
+	 * 확장자를 가져온다.</br> 파일명에 확장자를 포함하고 있을 경우, 확장자만 가져온다.
 	 * 
 	 * @param fileName
 	 *            파일명
 	 * @return 확장자
 	 */
 	static public String getExtension(String fileName) {
-		int index = fileName.lastIndexOf(".");
-		if (index < 0) {
+		int lastIndex = fileName.lastIndexOf(".");
+		if (lastIndex < 0) {
 			return null;
 		} else {
-			return fileName.substring(index + 1);
+			return fileName.substring(lastIndex + 1);
 		}
+	}
+
+	/**
+	 * 파일명을 가져온다.</br> 파일명에 경로명을 포함하고 있을 경우, 경로명을 제외한 파일명만 가져온다.
+	 * 
+	 * @param fileNameWithPath
+	 * @return
+	 */
+	static public String getFileName(String fileNameWithPath) {
+		int lastIndex = fileNameWithPath.lastIndexOf(System
+				.getProperty("file.separator"));
+		int beginIndex = (lastIndex > 0) ? lastIndex + 1 : 0;
+		String fileName = fileNameWithPath.substring(beginIndex);
+		logger.debug("fileNameWithPath: {}", fileNameWithPath);
+		logger.debug("realFileName: {}", fileName);
+		return fileName;
 	}
 }
