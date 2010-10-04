cf81c407caccd8ad042f045bb0da8c627b38cc51
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-10-04 11:00:55 +0000

    refectoring 반영

--- codelabor-system-xecure/src/main/java/org/codelabor/example/sign/xecure/web/struts/actions/ListAction.java
+++ codelabor-system-xecure/src/main/java/org/codelabor/example/sign/xecure/web/struts/actions/ListAction.java
@@ -10,7 +10,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.codelabor.system.sign.dtos.SignDTO;
 import org.codelabor.system.sign.managers.SignManager;
-import org.codelabor.system.struts.actions.BaseAction;
+import org.codelabor.system.web.struts.actions.BaseAction;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
