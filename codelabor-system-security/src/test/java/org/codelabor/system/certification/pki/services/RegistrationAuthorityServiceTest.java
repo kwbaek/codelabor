d3d144fac1c9ae588a0e7b1a6021cdbf4a1662b5
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-10-04 11:16:37 +0000

    refectoring 반영

--- codelabor-system-security/src/test/java/org/codelabor/system/certification/pki/services/RegistrationAuthorityServiceTest.java
+++ codelabor-system-security/src/test/java/org/codelabor/system/certification/pki/services/RegistrationAuthorityServiceTest.java
@@ -1,29 +1,40 @@
 package org.codelabor.system.certification.pki.services;
 
-import org.codelabor.system.test.BaseTestCase;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.codelabor.system.certification.pki.dtos.RegistrationDTO;
+import org.springframework.test.AbstractSingleSpringContextTests;
 
-public class RegistrationAuthorityServiceTest extends BaseTestCase {
-	@Autowired
-	protected RegistrationAuthorityService raService;
+public class RegistrationAuthorityServiceTest extends
+		AbstractSingleSpringContextTests {
+
+	private RegistrationAuthorityService registrationAuthorityService;
+
+	@Override
+	protected String[] getConfigLocations() {
+		return new String[] {
+				"classpath:/**/applicationContext-configurableCallBack.xml",
+				"classpath:/**/applicationContext-registrationAuthorityService.xml",
+				"classpath:/**/applicationContext-queryService-oracle.xml",
+				"classpath:/**/applicationContext-dataSourceService-oracle.xml" };
+	}
+
+	@Override
+	public void onSetUp() throws Exception {
+		registrationAuthorityService = (RegistrationAuthorityService) applicationContext
+				.getBean("registrationAuthorityService");
+	}
 
 	public void testEnroll() {
 		try {
-			// RegistrationDTO registrationDTO = new RegistrationDTO();
-			// registrationDTO.setId("bomber");
-			// registrationDTO.setPassword("bomber");
-			// registrationDTO.setRegistrationNumber("");
-			// raService.enroll(registrationDTO);
-			// System.out.println(registrationDTO);
+			RegistrationDTO registrationDTO = new RegistrationDTO();
+			registrationDTO.setId("bomber");
+			registrationDTO.setPassword("bomber");
+			registrationDTO.setRegistrationNumber("");
+			registrationAuthorityService.enroll(registrationDTO);
+			System.out.println(registrationDTO);
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
 	}
 
-	@Override
-	public void onSetUp() throws Exception {
-		raService = (RegistrationAuthorityService) applicationContext
-				.getBean("registrationAuthorityService");
-	}
 }
