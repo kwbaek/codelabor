cf1c43e442525b5643c9ce9b9fcb913b89892b25
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-10-05 05:24:42 +0000

    javadoc 보완

--- codelabor-system-security/src/main/java/org/codelabor/example/user/dtos/RoleDTO.java
+++ codelabor-system-security/src/main/java/org/codelabor/example/user/dtos/RoleDTO.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,29 +18,59 @@
 package org.codelabor.example.user.dtos;
 
 /**
+ * 역할 DTO
+ * 
  * @author Shin Sangjae
  * 
  */
 public class RoleDTO {
 
+	/**
+	 * 역할 Id
+	 */
+	protected String roleId = null;
+	/**
+	 * 상세 설명
+	 */
+	protected String description = null;
+
+	/**
+	 * 역할 Id를 가져온다.
+	 * 
+	 * @return 역할 Id
+	 */
 	public String getRoleId() {
 		return roleId;
 	}
 
+	/**
+	 * 역할 Id를 설정한다.
+	 * 
+	 * @param roleId
+	 *            역할 Id
+	 */
 	public void setRoleId(String roleId) {
 		this.roleId = roleId;
 	}
 
+	/**
+	 * 상세 설명을 가져온다.
+	 * 
+	 * @return 상세 설명
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * 상세 설명을 설정한다.
+	 * 
+	 * @param description
+	 *            상세 설명
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-	protected String roleId = null;
-	protected String description = null;
 
 	/**
 	 * Constructs a <code>String</code> with all attributes in name = value
--- codelabor-system-security/src/main/java/org/codelabor/example/user/dtos/UserDTO.java
+++ codelabor-system-security/src/main/java/org/codelabor/example/user/dtos/UserDTO.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,54 +18,102 @@
 package org.codelabor.example.user.dtos;
 
 /**
+ * 사용자 DTO
+ * 
  * @author Shin Sangjae
  * 
  */
 public class UserDTO {
 
+	/**
+	 * 사용자 Id
+	 */
+	protected String userId = null;
+	/**
+	 * 사용자명
+	 */
+	protected String userName = null;
+	/**
+	 * 패스워드
+	 */
+	protected String userPassword = null;
+
+	/**
+	 * 사용자 Id를 가져온다.
+	 * 
+	 * @return 사용자 Id
+	 */
 	public String getUserId() {
 		return userId;
 	}
+
+	/**
+	 * 사용자 Id를 설정한다.
+	 * 
+	 * @param userId
+	 *            사용자 Id
+	 */
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
+	/**
+	 * 패스워드를 가져온다.
+	 * 
+	 * @return 패스워드
+	 */
 	public String getUserPassword() {
 		return userPassword;
 	}
+
+	/**
+	 * 패스워드를 설정한다.
+	 * 
+	 * @param userPassword
+	 *            패스워드
+	 */
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
+
+	/**
+	 * 사용자명을 가져온다.
+	 * 
+	 * @return 사용자명
+	 */
 	public String getUserName() {
 		return userName;
 	}
+
+	/**
+	 * 사용자명을 설정한다.
+	 * 
+	 * @param userName
+	 *            사용자명
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	protected String userId = null;
-	protected String userName = null;
-	protected String userPassword = null;
+
 	/**
-	 * Constructs a <code>String</code> with all attributes
-	 * in name = value format.
-	 *
-	 * @return a <code>String</code> representation 
-	 * of this object.
+	 * Constructs a <code>String</code> with all attributes in name = value
+	 * format.
+	 * 
+	 * @return a <code>String</code> representation of this object.
 	 */
-	public String toString()
-	{
-	    final String TAB = "    ";
-	
-	    StringBuilder retValue = new StringBuilder();
-	    
-	    retValue.append("UserDTO ( ")
-	        .append(super.toString()).append(TAB)
-	        .append("userId = ").append(this.userId).append(TAB)
-	        .append("userName = ").append(this.userName).append(TAB)
-	        .append("userPassword = ").append(this.userPassword).append(TAB)
-	        .append(" )");
-	    
-	    return retValue.toString();
+	@Override
+	public String toString() {
+		final String TAB = "    ";
+
+		StringBuilder retValue = new StringBuilder();
+
+		retValue.append("UserDTO ( ").append(super.toString()).append(TAB)
+				.append("userId = ").append(this.userId).append(TAB).append(
+						"userName = ").append(this.userName).append(TAB)
+				.append("userPassword = ").append(this.userPassword)
+				.append(TAB).append(" )");
+
+		return retValue.toString();
 	}
-	
 
 }
