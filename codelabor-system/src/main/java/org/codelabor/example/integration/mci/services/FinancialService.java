a795063d026d1d7960616c6d1e10a4eede79fd95
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2010-12-17 08:18:20 +0000

    [maven-release-plugin]  copy for tag codelabor-system-mime-1.0.1

--- codelabor-system/src/main/java/org/codelabor/example/integration/mci/services/FinancialService.java
+++
@@ -1,9 +0,0 @@
-package org.codelabor.example.integration.mci.services;
-
-import java.util.Date;
-import java.util.List;
-
-public interface FinancialService {
-	@SuppressWarnings("unchecked")
-	List search(Date fromDate, Date toDate) throws Exception;
-}
--- java-example/src/org/codelabor/javaprogramming/homework/exam10/TimeClient.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.javaprogramming.homework.exam10;
-
-public interface TimeClient {
-	public void setTime(int hour, int minute, int second);
-	public void setDate(int day, int month, int year);
-	public void setDateAndTime(int day, int month, int year, int hour, int minute, int second);
-}
