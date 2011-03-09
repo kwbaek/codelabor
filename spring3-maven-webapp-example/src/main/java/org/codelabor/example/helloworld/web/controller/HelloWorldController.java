4ef976f7eb0158dbcd227941c1258345d2b31a43
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-03-09 01:13:26 +0000

    move

---
+++ spring3-maven-webapp-example/src/main/java/org/codelabor/example/helloworld/web/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package org.codelabor.example.helloworld.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/helloWorld")
+	public ModelAndView helloWorld() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/example/helloworld/helloWorld.jsp");
+		mav.addObject("message", "Hello, World!");
+		return mav;
+	}
+}
