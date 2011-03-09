7354b0b81d8ff0fb028d46ef2b26ca5268ad88c7
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-03-09 23:33:50 +0000

    Incomplete - task : Creating your first Controller

---
+++ spring3-maven-webapp-example/src/main/java/org/codelabor/example/helloworld/service/HelloWorldServiceImpl.java
@@ -0,0 +1,23 @@
+package org.codelabor.example.helloworld.service;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+@Service("HelloWorldService")
+public class HelloWorldServiceImpl implements HelloWorldService {
+
+	public String sayHello() throws Exception {
+		return "Hello, World!";
+	}
+
+	public String sayHello(String name) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hello, ");
+		if (StringUtils.isNotEmpty(name)) {
+			sb.append(name).append("!");
+		} else {
+			sb.append("World!");
+		}
+		return sb.toString();
+	}
+}
--- spring3-maven-webapp-example/src/main/java/org/codelabor/example/helloworld/web/controller/HelloWorldController.java
+++ spring3-maven-webapp-example/src/main/java/org/codelabor/example/helloworld/web/controller/HelloWorldController.java
@@ -1,33 +1,53 @@
 package org.codelabor.example.helloworld.web.controller;
 
+import org.codelabor.example.helloworld.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/helloWorld")
 public class HelloWorldController {
 
-	@RequestMapping("/helloWorld")
-	public ModelAndView helloWorld() {
+	@Autowired
+	private HelloWorldService helloWorldService;
+
+	@RequestMapping("/sayHello")
+	public ModelAndView sayHello(
+			@RequestParam(value = "name", required = false) String name)
+			throws Exception {
+		String message = helloWorldService.sayHello(name);
 		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", message);
 		mav.setViewName("/example/helloworld/helloWorld");
-		mav.addObject("message", "Hello, World!");
 		return mav;
 	}
 
-	@RequestMapping("/helloWorldPdf")
-	public ModelAndView helloWorldPdf() {
+	@RequestMapping("/sayHelloByPdf")
+	public ModelAndView sayHelloByPdf(
+			@RequestParam(value = "name", required = false) String name)
+			throws Exception {
+		String message = helloWorldService.sayHello(name);
 		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", message);
 		mav.setViewName("helloWorldPdfView");
-		mav.addObject("message", "Hello, World!");
 		return mav;
 	}
 
-	@RequestMapping("/helloWorldExcel")
-	public ModelAndView helloWorldExcel() {
+	@RequestMapping("/sayHelloByExcel")
+	public ModelAndView sayHelloByExcel(
+			@RequestParam(value = "name", required = false) String name)
+			throws Exception {
+		String message = helloWorldService.sayHello(name);
 		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", message);
 		mav.setViewName("helloWorldExcelView");
-		mav.addObject("message", "Hello, World!");
 		return mav;
+	}
+
+	public void setHelloWorldService(HelloWorldService helloWorldService) {
+		this.helloWorldService = helloWorldService;
 	}
 }
