4992a9fe0a2c754d6dd13b8ffec79090ae720095
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-04-15 09:02:06 +0000

    FilenameUtils 테스트 케이스 보완

--- commons-io-example/src/test/java/org/codelabor/example/io/FilenameUtilsTest.java
+++ commons-io-example/src/test/java/org/codelabor/example/io/FilenameUtilsTest.java
@@ -1,28 +1,57 @@
 package org.codelabor.example.io;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
 import org.apache.commons.io.FilenameUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FilenameUtilsTest extends TestCase {
+public class FilenameUtilsTest {
 
 	private final static Logger logger = LoggerFactory
 			.getLogger(FilenameUtilsTest.class);
+	String fullyQualifiedFileNameOnUnix = "/Users/Name/sample/test.txt";
+	String fullyQualifiedFileNameOnWindows = "C:\\Users\\Name\\sample\\test.txt";
 
-	@Override
+	@Before
 	public void setUp() throws Exception {
 	}
 
-	@Override
+	@After
 	public void tearDown() throws Exception {
 	}
 
-	public void testGetExtenstion() {
-		String extension = FilenameUtils.getExtension("xxxx");
+	@Test
+	public void testGetNameOnUnix() {
+		String name = FilenameUtils.getName(fullyQualifiedFileNameOnUnix);
+		logger.debug("name: {}", name);
+		assertEquals("test.txt", name);
+	}
+
+	@Test
+	public void testGetNameOnWindows() {
+		String name = FilenameUtils.getName(fullyQualifiedFileNameOnWindows);
+		logger.debug("name: {}", name);
+		assertEquals("test.txt", name);
+	}
+
+	@Test
+	public void testGetExtenstionOnWindows() {
+		String name = FilenameUtils.getName(fullyQualifiedFileNameOnUnix);
+		String extension = FilenameUtils.getExtension(name);
 		logger.debug("extension: {}", extension);
-		assertEquals("exe", extension);
+		assertEquals("txt", extension);
+	}
+
+	@Test
+	public void testGetExtenstionOnUnix() {
+		String name = FilenameUtils.getName(fullyQualifiedFileNameOnWindows);
+		String extension = FilenameUtils.getExtension(name);
+		logger.debug("extension: {}", extension);
+		assertEquals("txt", extension);
 	}
 
 }
