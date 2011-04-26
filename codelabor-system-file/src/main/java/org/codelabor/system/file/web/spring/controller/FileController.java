3129f5336a16f3b94b6b2a6b91fdcd8ffdfc7c6b
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-04-26 06:29:51 +0000

    @RequestParam에 required = false 추가

--- codelabor-system-file/src/main/java/org/codelabor/system/file/web/spring/controller/FileController.java
+++ codelabor-system-file/src/main/java/org/codelabor/system/file/web/spring/controller/FileController.java
@@ -16,23 +16,41 @@
  */
 package org.codelabor.system.file.web.spring.controller;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
+import org.codelabor.system.dto.StringIdArrayDTO;
+import org.codelabor.system.file.FileConstants;
 import org.codelabor.system.file.RepositoryType;
 import org.codelabor.system.file.dto.FileDTO;
+import org.codelabor.system.file.exception.InvalidRepositoryTypeException;
 import org.codelabor.system.file.manager.FileManager;
 import org.codelabor.system.file.util.UploadUtils;
 import org.codelabor.system.file.web.spring.command.FileList;
+import org.codelabor.system.web.servlet.HttpResponseHeaderConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import anyframe.common.util.StringUtil;
@@ -128,24 +146,191 @@ public class FileController {
 		return viewName;
 	}
 
-	public String download() {
-		String viewName = null;
-		return viewName;
+	@RequestMapping("/download")
+	public void download(@RequestHeader("User-Agent") String userAgent,
+			@RequestParam("fileId") String fileId, HttpServletResponse response)
+			throws Exception {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		FileDTO fileDTO;
+		fileDTO = fileManager.selectFileByFileId(fileId);
+		logger.debug("fileDTO: {}", fileDTO);
+
+		String repositoryPath = fileDTO.getRepositoryPath();
+		String uniqueFilename = fileDTO.getUniqueFilename();
+		String realFilename = fileDTO.getRealFilename();
+		InputStream inputStream = null;
+		if (StringUtil.isNotEmpty(repositoryPath)) {
+			// FILE_SYSTEM
+			stringBuilder.append(repositoryPath);
+			if (!repositoryPath.endsWith(File.separator)) {
+				stringBuilder.append(File.separator);
+			}
+			stringBuilder.append(uniqueFilename);
+			File file = new File(stringBuilder.toString());
+			inputStream = new FileInputStream(file);
+		} else {
+			// DATABASE
+			byte[] bytes = new byte[] {};
+			if (fileDTO.getFileSize() > 0) {
+				bytes = fileDTO.getBytes();
+			}
+			inputStream = new ByteArrayInputStream(bytes);
+
+		}
+		// set response contenttype, header
+		String encodedRealFilename = URLEncoder.encode(realFilename, "UTF-8");
+		logger.debug("realFilename: {}", realFilename);
+		logger.debug("encodedRealFilename: {}", encodedRealFilename);
+
+		response.setContentType(org.codelabor.system.file.FileConstants.CONTENT_TYPE);
+		stringBuilder.setLength(0);
+		if (userAgent.indexOf("MSIE5.5") > -1) {
+			stringBuilder.append("filename=");
+		} else {
+			stringBuilder.append("attachment; filename=");
+		}
+		// stringBuilder.append("\"");
+		stringBuilder.append(encodedRealFilename);
+		// stringBuilder.append("\"");
+		response.setHeader(HttpResponseHeaderConstants.CONTENT_DISPOSITION,
+				stringBuilder.toString());
+
+		logger.debug("header: {}", stringBuilder.toString());
+		logger.debug("character encoding: {}", response.getCharacterEncoding());
+		logger.debug("content type: {}", response.getContentType());
+		logger.debug("bufferSize: {}", response.getBufferSize());
+		logger.debug("locale: {}", response.getLocale());
+
+		BufferedInputStream bufferdInputStream = new BufferedInputStream(
+				inputStream);
+		ServletOutputStream servletOutputStream = response.getOutputStream();
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+				servletOutputStream);
+		int bytesRead;
+		byte buffer[] = new byte[2048];
+		while ((bytesRead = bufferdInputStream.read(buffer)) != -1) {
+			bufferedOutputStream.write(buffer, 0, bytesRead);
+		}
+		// flush stream
+		bufferedOutputStream.flush();
+
+		// close stream
+		inputStream.close();
+		bufferdInputStream.close();
+		servletOutputStream.close();
+		bufferedOutputStream.close();
 	}
 
-	public String view() {
-		String viewName = null;
-		return viewName;
+	@RequestMapping("/view")
+	public void view(@RequestParam("fileId") String fileId,
+			HttpServletResponse response) throws Exception {
+		StringBuilder stringBuilder = null;
+
+		FileDTO fileDTO;
+		fileDTO = fileManager.selectFileByFileId(fileId);
+		logger.debug("fileDTO: {}", fileDTO);
+
+		String repositoryPath = fileDTO.getRepositoryPath();
+		String uniqueFilename = fileDTO.getUniqueFilename();
+		String realFilename = fileDTO.getRealFilename();
+		InputStream inputStream = null;
+		if (StringUtil.isNotEmpty(repositoryPath)) {
+			// FILE_SYSTEM
+			stringBuilder = new StringBuilder();
+			stringBuilder.append(repositoryPath);
+			if (!repositoryPath.endsWith(File.separator)) {
+				stringBuilder.append(File.separator);
+			}
+			stringBuilder.append(uniqueFilename);
+			File file = new File(stringBuilder.toString());
+			inputStream = new FileInputStream(file);
+		} else {
+			// DATABASE
+			byte[] bytes = new byte[] {};
+			if (fileDTO.getFileSize() > 0) {
+				bytes = fileDTO.getBytes();
+			}
+			inputStream = new ByteArrayInputStream(bytes);
+
+		}
+		response.setContentType(fileDTO.getContentType());
+
+		// set response contenttype, header
+		String encodedRealFilename = URLEncoder.encode(realFilename, "UTF-8");
+		logger.debug("realFilename: {}", realFilename);
+		logger.debug("encodedRealFilename: {}", encodedRealFilename);
+		logger.debug("character encoding: {}", response.getCharacterEncoding());
+		logger.debug("content type: {}", response.getContentType());
+		logger.debug("bufferSize: {}", response.getBufferSize());
+		logger.debug("locale: {}", response.getLocale());
+
+		BufferedInputStream bufferdInputStream = new BufferedInputStream(
+				inputStream);
+		ServletOutputStream servletOutputStream = response.getOutputStream();
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+				servletOutputStream);
+		int bytesRead;
+		byte buffer[] = new byte[2048];
+		while ((bytesRead = bufferdInputStream.read(buffer)) != -1) {
+			bufferedOutputStream.write(buffer, 0, bytesRead);
+		}
+		// flush stream
+		bufferedOutputStream.flush();
+
+		// close stream
+		inputStream.close();
+		bufferdInputStream.close();
+		servletOutputStream.close();
+		bufferedOutputStream.close();
 	}
 
-	public String list() {
-		String viewName = null;
-		return viewName;
+	@RequestMapping("/list")
+	public String list(
+			@RequestParam(value = "mapId", required = false) String mapId,
+			@RequestParam(value = "repositoryType", required = false) String repositoryType,
+			Model model) {
+		List<FileDTO> fileDTOList = null;
+		try {
+			if (StringUtils.isEmpty(repositoryType)) {
+				if (StringUtils.isEmpty(mapId)) {
+					fileDTOList = fileManager.selectFileAll();
+				} else {
+					fileDTOList = fileManager.selectFileByMapId(mapId);
+				}
+			} else {
+				switch (RepositoryType.valueOf(repositoryType)) {
+				case DATABASE:
+					fileDTOList = fileManager
+							.selectFileByRepositoryType(RepositoryType.DATABASE);
+					break;
+				case FILE_SYSTEM:
+					fileDTOList = fileManager
+							.selectFileByRepositoryType(RepositoryType.FILE_SYSTEM);
+					break;
+				default:
+					logger.error("Invalid repository type: {}", repositoryType);
+					throw new InvalidRepositoryTypeException(repositoryType);
+				}
+
+			}
+			model.addAttribute(FileConstants.FILE_LIST_KEY, fileDTOList);
+			model.addAttribute(FileConstants.MAP_ID,
+					mapIdGenerationService.getNextStringId());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage());
+		}
+		return "example.file.spring.mvc.list.definition";
 	}
 
-	public String delete() {
-		String viewName = null;
-		return viewName;
+	@RequestMapping("/delete")
+	public String delete(StringIdArrayDTO springIDArrayDTO) throws Exception {
+		String[] idArray = springIDArrayDTO.getId();
+		logger.debug("file id: {}", Arrays.asList(idArray));
+		fileManager.deleteFileByFileId(idArray);
+		return "redirect:/example/file/spring/mvc/list.do";
 	}
 
 }
