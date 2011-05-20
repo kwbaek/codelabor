ec7f154169708130ee347e1f7a45e902eeb2576f
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-05-20 06:09:55 +0000

    [maven-release-plugin]  copy for tag codelabor-system-core-3.0.5

--- java-example/src/org/codelabor/javaprogramming/homework/exam11/shapepackage/Shape.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.javaprogramming.homework.exam11.shapepackage;
-
-public abstract class Shape {
-	public abstract double getCircumference();
-	public abstract double getWidth();
-	public abstract double getSquare();
-}
