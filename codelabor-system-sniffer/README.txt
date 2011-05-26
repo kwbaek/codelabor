591f33792df7364b965ccac1660be6ac80b6f5ba
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-05-26 10:09:23 +0000

    [maven-release-plugin]  copy for tag codelabor-system-pattern-3.0.3

--- codelabor-system-sniffer/README.txt
+++
@@ -1,9 +0,0 @@
-2.0.2
-	SniffingHttpSessionListner 추가
-
-2.0.3
-	로그 보완 (시간 포맷)
-
-2.0.4
-	dumpElapsedTime bug fix
-	encoding 설정 보완
--- codelabor-system/src/main/java/org/codelabor/system/security/HashFunctionType.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.system.security;
-
-public enum HashFunctionType {
-	// Registry Name: Hash Function Textual Names
-	// Reference: [RFC4572]
-	MD2, MD5, SHA1, SHA224, SHA256, SHA384, SHA512, HAS160;
-}
