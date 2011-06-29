242c0a38222a3692318c86c25817513c844077af
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2011-06-29 04:05:55 +0000

    [maven-release-plugin]  copy for tag codelabor-system-file-spring-mvc-3.1.0

--- registration-authority-service/src/main/java/org/codelabor/system/certification/pki/services/RegistrationAuthorityService.java
+++
@@ -1,9 +0,0 @@
-package org.codelabor.system.certification.pki.services;
-
-import org.codelabor.system.certification.pki.CertificateType;
-import org.codelabor.system.certification.pki.dtos.CodeDTO;
-
-
-public interface RegistrationAuthorityService {
-	public CodeDTO enroll(String name, String registrationNumber, String dn, CertificateType certificateType, boolean isNew) throws Exception;
-}
