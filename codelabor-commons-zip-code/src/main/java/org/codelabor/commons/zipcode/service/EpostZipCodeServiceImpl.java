f736a521d78269f6714b3e1194cb56fbf2c4fe7a
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2012-06-13 06:24:49 +0000

    logFile null check

--- codelabor-commons-zip-code/src/main/java/org/codelabor/commons/zipcode/service/EpostZipCodeServiceImpl.java
+++ codelabor-commons-zip-code/src/main/java/org/codelabor/commons/zipcode/service/EpostZipCodeServiceImpl.java
@@ -1,5 +1,20 @@
 package org.codelabor.commons.zipcode.service;
 
 public class EpostZipCodeServiceImpl implements ZipCodeService {
+	protected String openApiRegKey = null;
 
+	/**
+	 * @return the openApiRegKey
+	 */
+	public String getOpenApiRegKey() {
+		return openApiRegKey;
+	}
+
+	/**
+	 * @param openApiRegKey
+	 *            the openApiRegKey to set
+	 */
+	public void setOpenApiRegKey(String openApiRegKey) {
+		this.openApiRegKey = openApiRegKey;
+	}
 }
