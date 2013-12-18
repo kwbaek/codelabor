0524f16cdbdee5b48cdce0a6f6ead9d372b8d02c
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2013-12-18 02:22:51 +0000

    [maven-release-plugin]  copy for tag codelabor-common-0.0.1

--- registration-authority-service/src/main/java/org/codelabor/system/certification/pki/services/signkorea/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.system.certification.pki.services.signkorea;
-
-public class Constants {
-	public final static String testPrivateDNPrefix = "(policy=sign-Platinum-PB)";
-	public final static String testCorporationDNPrefix = "(policy=sign-Platinum-CB)";
-	public final static String defaultDN = "c=KR";
-}
