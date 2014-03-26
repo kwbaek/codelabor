011425a6c861186fde0569f2ef3a0f8e3bdb1fc8
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2014-03-26 12:28:42 +0000

    [maven-release-plugin]  copy for tag codelabor-system-core-4.0.1

--- codelabor-prototype/src/main/java/org/codelabor/example/username/service/UsernameService.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.example.username.service;
-
-import org.codelabor.example.username.dto.RegistrationFormDto;
-
-public interface UsernameService {
-	int registration(RegistrationFormDto registrationFormDto);
-}
--- codelabor-system-mock/src/main/java/org/codelabor/system/mock/services/EchoService.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.system.mock.services;
-
-public interface EchoService {
-	public String[] echo(String[] array) throws Exception;
-
-	public Object echo(Object obj) throws Exception;
-}
