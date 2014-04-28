e7a938beb8a6b87b9664713c65dcb054cbfc2179
codelabor <codelabor@28bd6c16-bb75-11dd-ad1f-f1f9622dbc98>
2014-04-28 14:24:09 +0000

    [maven-release-plugin]  copy for tag codelabor-system-web-4.0.4

--- codelabor-example-lifecycle/src/main/java/org/codelabor/example/lifecycle/context/UserContext.java
+++
@@ -1,7 +0,0 @@
-package org.codelabor.example.lifecycle.context;
-
-public interface UserContext {
-	public String getUserId();
-
-	public void setUserId(String userId);
-}
--- codepro-example/src/main/java/org/codelabor/example/codepro/performance/service/AppendStringService.java
+++
@@ -1,9 +0,0 @@
-package org.codelabor.example.codepro.performance.service;
-
-public interface AppendStringService {
-
-	public abstract String appendStringLiteral(String data);
-
-	public abstract String appendCharacterLiteral(String data);
-
-}
